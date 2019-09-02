f5f36108414b3d40349dc3d3541b06b3dfb9cc3f
Julian Benitez <julian.benitez@mail.escuelaing.edu.co>
2019-09-01 20:16:23 -0500

    Prints Main

--- BLUEPRINTS-PART1/src/main/java/edu/eci/arsw/blueprints/programa/MainPrograma.java
+++ BLUEPRINTS-PART1/src/main/java/edu/eci/arsw/blueprints/programa/MainPrograma.java
@@ -27,21 +27,29 @@ public class MainPrograma {
         Point[] pts0=new Point[]{new Point(40, 40),new Point(15, 15)};
         Blueprint bp0=new Blueprint("mack", "mypaint",pts0);
         Point[] pts1=new Point[]{new Point(1, 1),new Point(11, 11)};
-        Blueprint bp1=new Blueprint("john ripper", "pepito",pts1);
+        Blueprint bp1=new Blueprint("jack", "thestatue",pts1);
         Point[] pts2=new Point[]{new Point(2, 2),new Point(12, 12)};
-        Blueprint bp2=new Blueprint("juanes", "pepe",pts2);
+        Blueprint bp2=new Blueprint("juanes", "thepaint2.0",pts2);
         Point[] pts3=new Point[]{new Point(3, 3),new Point(13, 13)};
-        Blueprint bp3=new Blueprint("juanes", "camisa negra",pts2);
+        Blueprint bp3=new Blueprint("juanes", "thestatue2.0",pts2);
         
         bps.addNewBlueprint(bp);
+        System.out.println("Añadiendo "+bp);
         bps.addNewBlueprint(bp0);
+        System.out.println("Añadiendo "+bp0);
         bps.addNewBlueprint(bp1);
+        System.out.println("Añadiendo "+bp1);
         bps.addNewBlueprint(bp2);
+        System.out.println("Añadiendo "+bp2);
         bps.addNewBlueprint(bp3);
-        
-        System.out.println(bps.getBlueprint("john","thepaint").toString());
-        System.out.println(bps.getBlueprintsByAuthor("juanes").toString());
-        System.out.println(bps.getAllBlueprints().toString());
+        System.out.println("Añadiendo "+bp3);
+        System.out.println();
+        System.out.println("Obteniendo la blueprint john/thepaint");
+        System.out.println(bps.getBlueprint("john","thepaint"));
+        System.out.println("Obteniendo blueprints de juanes");
+        System.out.println(bps.getBlueprintsByAuthor("juanes"));
+        System.out.println("Obteniendo todas las blueprints");
+        System.out.println(bps.getAllBlueprints());
         
     }
     
